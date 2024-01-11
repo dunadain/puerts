62156862bcff8388ad8e0d9b5bda123e79c078a8
Ryan Sampras <maelendil@gmail.com>
2024-01-12 01:23:29 +0800

    Update log.mjs

--- unity/Assets/core/upm/Runtime/Resources/puerts/log.mjs
+++ unity/Assets/core/upm/Runtime/Resources/puerts/log.mjs
@@ -3,7 +3,7 @@
  * Copyright (C) 2020 THL A29 Limited, a Tencent company.  All rights reserved.
  * Puerts is licensed under the BSD 3-Clause License, except for the third-party components listed in the file 'LICENSE' which may be subject to their corresponding license terms. 
  * This file is subject to the terms and conditions defined in file 'LICENSE', which is part of this source code package.
- */ 
+ */
 
 var global = global || globalThis || (function () { return this; }());
 let UnityEngine_Debug = puer.loadType('UnityEngine.Debug');
@@ -26,7 +26,7 @@ if (UnityEngine_Debug || !global.console) {
 
     const console_org = global.console;
     var console = {}
-    
+
     function toString(args) {
         return Array.prototype.map.call(args, x => {
             try {
@@ -36,34 +36,39 @@ if (UnityEngine_Debug || !global.console) {
             }
         }).join(' ');
     }
-    
+
     function getStack(error) {
         let stack = error.stack; // get js stack
         stack = stack.substring(stack.indexOf("\n")+1); // remove first line ("Error")
         stack = stack.replace(/^ {4}/gm, ""); // remove indentation
         return stack
     }
-    
+
     console.log = function() {
         if (console_org) console_org.log.apply(null, Array.prototype.slice.call(arguments));
         UnityEngine_Debug.Log(toString(arguments));
     }
-    
+
+    console.logWithColor = function (msg, color) {
+        if (console_org) console_org.log.call(null, `%c${msg}`, `color: ${color}`);
+        UnityEngine_Debug.Log(`<color=${color}>${msg}</color>`);
+    }
+
     console.info = function() {
         if (console_org) console_org.info.apply(null, Array.prototype.slice.call(arguments));
         UnityEngine_Debug.Log(toString(arguments));
     }
-    
+
     console.warn = function() {
         if (console_org) console_org.warn.apply(null, Array.prototype.slice.call(arguments));
         UnityEngine_Debug.LogWarning(toString(arguments));
     }
-    
+
     console.error = function() {
         if (console_org) console_org.error.apply(null, Array.prototype.slice.call(arguments));
         UnityEngine_Debug.LogError(toString(arguments));
     }
-    
+
     console.trace = function() {
         if (console_org) console_org.trace.apply(null, Array.prototype.slice.call(arguments));
         UnityEngine_Debug.Log(toString(arguments) + "\n" + getStack(new Error()) + "\n");
@@ -92,7 +97,7 @@ if (UnityEngine_Debug || !global.console) {
             console.warn("Timer '" + String(name)+ "' does not exist");
         };
     }
-    
+
     global.console = console;
     puer.console = console;
 
